5507dccb74abf07cdf55b2afb944927f9aa9714d
Will Williams <wwilliams@indeed.com>
2023-10-23 22:34:19 -0700

    3.12.3: Rule 2 - Immutable Updates

--- updateStudentImmutably.js
+++ updateStudentImmutably.js
@@ -1,3 +1,16 @@
+const student = {
+  id: 1,
+  firstName: "Ahmad",
+  lastName: "Salim",
+  enrolled: false,
+};
+
+/** Add your code below */
+
+// --------------------------------------------------------------------
+// Method 2 (CORRECT): Update by returning a NEW, IMMUTABLE COPY of the state
+// --------------------------------------------------------------------
+
 const updateStudentImmutably = (student) => {
   return {
     ...student,
@@ -5,11 +18,22 @@ const updateStudentImmutably = (student) => {
   };
 };
 
-const student = {
-  id: 1,
-  firstName: 'Ahmad',
-  lastName: 'Salim',
-  enrolled: false,
-};
+const updatedStudent = updateStudentImmutably(student);
 
-/** Add your code below */
+console.log(
+  `Method 2 (CORRECT): Update by returning a NEW, IMMUTABLE COPY of the state
+  
+  student should show enrolled: 'false' and will correctly show 'false' bc student
+  was copied to a NEW object using fn updateStudentImmutably:
+  `
+);
+console.log(
+  `student - should be 'false': \n${JSON.stringify(student, null, 2)}`
+);
+console.log(
+  `updatedStudent - should be 'true': \n${JSON.stringify(
+    updatedStudent,
+    null,
+    2
+  )}`
+);
--- updateStudentMutably.js
+++ updateStudentMutably.js
@@ -1,13 +1,38 @@
+const student = {
+  id: 1,
+  firstName: "Ahmad",
+  lastName: "Salim",
+  enrolled: false,
+};
+
+/** Add your code below */
+
+// --------------------------------------------------------------------
+// Method 1 (INCORRECT): Updates by mutating the (existing) state directly
+// --------------------------------------------------------------------
+
 const updateStudentMutably = (student) => {
   student.enrolled = !student.enrolled;
   return student;
 };
 
-const student = {
-  id: 1,
-  firstName: 'Ahmad',
-  lastName: 'Salim',
-  enrolled: false,
-};
+const updatedStudent = updateStudentMutably(student);
 
-/** Add your code below */
+console.log(
+  `Method 1 (INCORRECT): Updates by mutating the (existing) state directly:
+  
+  student should show enrolled: 'false' but will instead show 'true' bc student 
+  was mutated directly using fn updateStudentMutably:
+  `
+);
+
+console.log(
+  `student - should be 'false': \n${JSON.stringify(student, null, 2)}`
+);
+console.log(
+  `updatedStudent - should be 'true': \n${JSON.stringify(
+    updatedStudent,
+    null,
+    2
+  )}`
+);
