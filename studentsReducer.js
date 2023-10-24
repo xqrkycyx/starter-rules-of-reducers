e9ea22f9928f26f1999ca4cd02f673d963b45956
Will Williams <wwilliams@indeed.com>
2023-10-23 22:07:46 -0700

    3.12.3: Rule 1 - compute using only initial state + action

--- studentsReducer.js
+++ studentsReducer.js
@@ -1,16 +1,16 @@
 const initialState = [
-  { id: 1, firstName: 'Ahmad', lastName: 'Salim', enrolled: false },
-  { id: 2, firstName: 'Gabriel', lastName: 'Carlos', enrolled: true },
-  { id: 3, firstName: 'Cassandra', lastName: 'Williams', enrolled: false },
+  { id: 1, firstName: "Ahmad", lastName: "Salim", enrolled: false },
+  { id: 2, firstName: "Gabriel", lastName: "Carlos", enrolled: true },
+  { id: 3, firstName: "Cassandra", lastName: "Williams", enrolled: false },
 ];
 
 const studentsReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'students/addStudent': {
+    case "students/addStudent": {
       return [...state, action.payload];
     }
-    case 'students/removeStudent': {
-      return state.filter((note) => note.id !== action.payload.id);
+    case "students/removeStudent": {
+      return state.filter((student) => student.id !== action.payload.id);
     }
     default: {
       return state;
@@ -19,3 +19,29 @@ const studentsReducer = (state = initialState, action) => {
 };
 
 /** Add your code below */
+const secondState = studentsReducer(initialState, {
+  type: "students/addStudent",
+  payload: {
+    id: 4,
+    firstName: "Ming",
+    lastName: "Chan",
+    enrolled: true,
+  },
+});
+
+console.log(`initialState: \n${JSON.stringify(initialState, null, 2)}`);
+
+console.log(`secondState (addStudent): \n${JSON.stringify(secondState, null, 2)}
+`);
+
+const thirdState = studentsReducer(secondState, {
+  type: "students/removeStudent",
+  payload: { id: 2 },
+});
+
+console.log(`thirdState (removesStudent): \n${JSON.stringify(
+  thirdState,
+  null,
+  2
+)}
+`);
